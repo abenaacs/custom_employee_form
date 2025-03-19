00fcc38a167bd22f97748887bbf2d34873b04ea4
Abenezer <abenezernigussiecs@gmail.com>
2025-03-19 14:41:28 +0300

    Custom file uploads and field removals for Employee module foudnation has been laid

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Employee Customization",
+    "version": "1.0",
+    "summary": "Custom file uploads and field removals for Employee module",
+    "description": "This module adds custom file upload fields and removes unnecessary fields from the Employee form",
+    "author": "Abenezer Nigussie",
+    "depends": ["hr"],
+    "data": [
+        "views/hr_employee_inherit_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
