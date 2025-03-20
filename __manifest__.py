199e906b6e0519ceb0d23c3756b20653f54051bf
Abenezer <abenezernigussiecs@gmail.com>
2025-03-20 16:28:49 +0300

    New update incuding controller and enhanced model and security along with the user interface

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,16 @@
 {
     "name": "Employee Customization",
     "version": "1.0",
+    'category': 'Human Resources',
     "summary": "Custom file uploads and field removals for Employee module",
     "description": "This module adds custom file upload fields and removes unnecessary fields from the Employee form",
     "author": "Abenezer Nigussie",
-    "depends": ["hr"],
-    "data": [
-        "views/hr_employee_inherit_views.xml",
+    "depends": ['hr'],
+    'data': [
+        'views/employee_view.xml',
+        'security/ir.model.access.csv',
     ],
     "installable": True,
     "application": False,
+    'license': 'LGPL-3',
 }
